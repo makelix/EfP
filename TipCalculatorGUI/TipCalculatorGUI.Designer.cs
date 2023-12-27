85a15d6fc61c6ceabf0415fd37ac4c01101880b1
Marko Haverinen <marko.haverinen@savox.com>
2023-12-27 15:56:58 +0200

    Add TipCalculatorGUI.

---
+++ TipCalculatorGUI/TipCalculatorGUI.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace TipCalculatorGUI
+{
+    partial class TipCalculatorGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label2;
+            System.Windows.Forms.Label label3;
+            System.Windows.Forms.Label label4;
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.BillAmount = new System.Windows.Forms.TextBox();
+            this.TipRate = new System.Windows.Forms.TextBox();
+            this.Tip = new System.Windows.Forms.Label();
+            this.Total = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(13, 10);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(58, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Bill amount";
+            // 
+            // label2
+            // 
+            label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(13, 42);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(58, 32);
+            label2.TabIndex = 1;
+            label2.Text = "Tip rate";
+            // 
+            // label3
+            // 
+            label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(13, 74);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(58, 32);
+            label3.TabIndex = 2;
+            label3.Text = "Tip";
+            // 
+            // label4
+            // 
+            label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(13, 106);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(58, 33);
+            label4.TabIndex = 3;
+            label4.Text = "Total";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.BillAmount, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.TipRate, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Tip, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.Total, 1, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(10);
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(299, 149);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // BillAmount
+            // 
+            this.BillAmount.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BillAmount.Location = new System.Drawing.Point(77, 13);
+            this.BillAmount.Name = "BillAmount";
+            this.BillAmount.Size = new System.Drawing.Size(209, 20);
+            this.BillAmount.TabIndex = 4;
+            // 
+            // TipRate
+            // 
+            this.TipRate.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TipRate.Location = new System.Drawing.Point(77, 45);
+            this.TipRate.Name = "TipRate";
+            this.TipRate.Size = new System.Drawing.Size(209, 20);
+            this.TipRate.TabIndex = 5;
+            // 
+            // Tip
+            // 
+            this.Tip.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Tip.AutoSize = true;
+            this.Tip.Location = new System.Drawing.Point(77, 74);
+            this.Tip.Name = "Tip";
+            this.Tip.Size = new System.Drawing.Size(209, 32);
+            this.Tip.TabIndex = 6;
+            this.Tip.Text = "$0.00";
+            // 
+            // Total
+            // 
+            this.Total.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Total.AutoSize = true;
+            this.Total.Location = new System.Drawing.Point(77, 106);
+            this.Total.Name = "Total";
+            this.Total.Size = new System.Drawing.Size(209, 33);
+            this.Total.TabIndex = 7;
+            this.Total.Text = "$0.00";
+            // 
+            // TipCalculatorGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 149);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TipCalculatorGUI";
+            this.Text = "TipCalculator";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox BillAmount;
+        private System.Windows.Forms.TextBox TipRate;
+        private System.Windows.Forms.Label Tip;
+        private System.Windows.Forms.Label Total;
+    }
+}
+
