e4d173c715ad7cc35c5d094c73a5b2aad1f3880b
Marko Haverinen <marko.haverinen@savox.com>
2023-12-29 21:26:26 +0200

    First version of PrintQuote.

---
+++ PrintQuote/PrintQuote.cs
@@ -0,0 +1,9 @@
+﻿internal class PrintQuote
+{
+    static void Main(string[] args)
+    {
+        string quote= Utilities.AskForString("What is the quote? ");
+        string author = Utilities.AskForString("Who said it? ");
+        Console.WriteLine(author + " says, \"" + quote + "\"");
+    }
+}
