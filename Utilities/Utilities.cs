a23ab2899b3bae39bc73b5e6e1440ca2d7129ee6
Marko Haverinen <marko.haverinen@savox.com>
2023-12-29 22:17:28 +0200

    Implement Utilities.AskForInt().

--- Utilities/Utilities.cs
+++ Utilities/Utilities.cs
@@ -4,7 +4,7 @@
     /// Request a string from the user on commandline.
     /// </summary>
     /// <param name="prompt">The prompt presented to the user.</param>
-    /// <param name="isMandatory">If true, the user must provide a valid strin.</param>
+    /// <param name="isMandatory">If true, the user must provide a valid string.</param>
     /// <param name="defaultValue">The defaultValue is returned when isMandatory == false
     ///                            and the user doesn't provide an answer.</param>
     /// <returns>The string provided by the user or the defaultValue.</returns>
@@ -31,9 +31,52 @@
         }
     }
 
+    /// <summary>
+    /// Request an integer from the user on commandline.
+    /// </summary>
+    /// <param name="prompt">The prompt presented to the user.</param>
+    /// <param name="minValue">The minimum accepted value.</param>
+    /// <param name="maxValue">The maximum accepted value.</param>
+    /// <param name="isMandatory">If true, the user must provide a valid string.</param>
+    /// <param name="defaultValue">The defaultValue is returned when isMandatory == false
+    ///                            and the user doesn't provide an answer. The defaultValue
+    ///                            can be outside the minimum and maximum values.
+    /// <returns>The integer provided by the user or the defaultValue.</returns>
+    public static int AskForInt(string prompt,
+                                int minValue = int.MinValue,
+                                int maxValue = int.MaxValue,
+                                bool isMandatory = true,
+                                int defaultValue = 0)
+    {
+        while (true)
+        {
+            Console.Write(prompt);
+            string? input = Console.ReadLine();
+
+            // If we didn't receive input...
+            if (String.IsNullOrEmpty(input))
+            {
+                // ...but input is mandatory, ask again.
+                if (isMandatory)
+                    continue;
+
+                // If input isn't mandatory, return the default value.
+                return defaultValue;
+            }
+
+            // We received a string - if it's a valid number between
+            // minValue and maxValue, return it.
+            if (int.TryParse(input, out int result))
+            {
+                if (minValue <= result && result <= maxValue)
+                    return result;
+            }
+        }
+    }
+
     public static void Main()
     {
-        string input = AskForString("Type a string: ");
+        int input = AskForInt("Type a number: ");
         Console.WriteLine($"You typed: {input}");
     }
 }
