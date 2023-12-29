ca38aac1c39c26c49dadd7423d7804d6e14d37bc
Marko Haverinen <marko.haverinen@savox.com>
2023-12-29 22:32:49 +0200

    First version of RetirementCalculator.

---
+++ RetirementCalculator/RetirementCalculator.cs
@@ -0,0 +1,15 @@
+﻿internal class RetirementCalculator
+{
+    static void Main(string[] args)
+    {
+        int age = Utilities.AskForInt("What is your current age? ");
+        int retirementAge = Utilities.AskForInt("At what age would you like to retire? ");
+
+        int yearsLeft = retirementAge - age;
+        int currentYear = DateTime.Now.Year;
+        int retirementYear = currentYear + yearsLeft;
+
+        Console.WriteLine($"You have {yearsLeft} years left until you can retire.");
+        Console.WriteLine($"It's {currentYear}, so you can retire in {retirementYear}.");
+    }
+}
