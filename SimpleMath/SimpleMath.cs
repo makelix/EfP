240a0e2db2d4d83e074cc93cd194961907346001
Marko Haverinen <marko.haverinen@savox.com>
2023-12-29 22:23:23 +0200

    First version of SimpleMath.

---
+++ SimpleMath/SimpleMath.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+internal class SimpleMath
+{
+    static void Main(string[] args)
+    {
+        int first = Utilities.AskForInt("What is the first number? ");
+        int second = Utilities.AskForInt("What is the second number? ");
+        StringBuilder sb = new();
+        sb.Append($"{first} + {second} = {first + second}\n");
+        sb.Append($"{first} - {second} = {first - second}\n");
+        sb.Append($"{first} * {second} = {first * second}\n");
+        sb.Append($"{first} / {second} = {first / second}");
+        Console.WriteLine(sb.ToString());
+    }
+}
