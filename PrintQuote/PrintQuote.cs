c7ffd68998477ca700298f87d0c8b5c40da9095c
Marko Haverinen <marko.haverinen@savox.com>
2023-12-29 21:51:47 +0200

    Implement PrintQuote using List<KeyValuePair<string, string>>.

--- PrintQuote/PrintQuote.cs
+++ PrintQuote/PrintQuote.cs
@@ -1,9 +1,24 @@
-﻿internal class PrintQuote
+﻿using System.Collections;
+using System.Collections.Generic;
+
+internal class PrintQuote
 {
     static void Main(string[] args)
     {
-        string quote= Utilities.AskForString("What is the quote? ");
-        string author = Utilities.AskForString("Who said it? ");
-        Console.WriteLine(author + " says, \"" + quote + "\"");
+        List<KeyValuePair<string, string>> quotes = [];
+        while (true)
+        {
+            string quote= Utilities.AskForString("What is the quote? ", false);
+            if (String.IsNullOrEmpty(quote))
+                break;
+            string author = Utilities.AskForString("Who said it? ");
+            KeyValuePair<string, string> kvp = new(author, quote);
+            quotes.Add(kvp);
+        }
+
+        foreach (var quote in quotes)
+        {
+            Console.WriteLine(quote.Key + " says, \"" + quote.Value + "\"");
+        }
     }
 }
