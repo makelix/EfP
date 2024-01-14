a54be8b87049ddc5f709066d927006cfb576700e
Marko Haverinen <marko.haverinen@gmail.com>
2024-01-14 11:56:48 +0200

    First version of CurrencyConverter.

--- CurrencyConverter/CurrencyConverter.cs
+++ CurrencyConverter/CurrencyConverter.cs
@@ -1,7 +1,24 @@
-﻿public class CurrencyConverter
+﻿
+
+public class CurrencyConverter
 {
+    private decimal _exchangeRate = 1.0942M;
+
+    public decimal Convert(decimal amountFrom)
+    {
+        if (amountFrom < 0) { throw new ArgumentOutOfRangeException("Amount must be positive.") ; }
+        return Math.Round((amountFrom * 1) / _exchangeRate, 2, MidpointRounding.AwayFromZero);
+    }
+
+    public void SetRate(decimal exchangeRate = 1.0942M) => _exchangeRate = exchangeRate;
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CurrencyConverter converter = new();
+        decimal amountFrom = Utilities.AskForDecimal("How many euros are you exchanging? ", 0M);
+        decimal exchangeRate = Utilities.AskForDecimal("What is the exchange rate? ", 0);
+        converter.SetRate(exchangeRate);
+        decimal amountTo = converter.Convert(amountFrom);
+        Console.WriteLine($"{amountFrom} euros at an exchange rate of {exchangeRate} is\n{amountTo} U.S. dollars.");
     }
 }
